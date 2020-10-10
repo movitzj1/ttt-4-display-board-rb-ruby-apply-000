c288ec22ec5024c7c8c10365f8b6fea5e1c00731
Justin Movitz <movitzj1@montclair.edu>
2020-10-10 20:32:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 board = ["  O  ", "  X  ", "    ", "    ", "  X  ", "    ", "  X  ", "  O  ", "    "]
 def display_board(board)
-  puts board [0]
-  puts board [1]
-  puts board [2]
-  puts 
+  puts board [0] + "|" + board [1] + "|" + board [2]
+  puts -----------
+  puts board [3] + "|" + board [4] + "|" + board [5]
+  puts -----------
+  puts board [6] + "|" + board [7] + "|" + board [8]
 end
